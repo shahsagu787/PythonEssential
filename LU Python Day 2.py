86dfefd46f7e88f1f552527ba1ac9c7737c3adb0
shahsagu787 <68490794+shahsagu787@users.noreply.github.com>
2020-07-21 20:24:06 +0530

    Add files via upload

---
+++ LU Python Day 2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[6]:
+
+
+print("hello       python")
+
+
+# In[12]:
+
+
+print("python's world")
+
+
+# In[14]:
+
+
+print('Python\'s world')
+
+
+# In[16]:
+
+
+x=10
+y=20
+print(x)
+print(y)
+print(x>y, x<y, x<=y, x>=y)
+
+print(x+y, x-y, x//y, x/y, x**2, y%x)
+
+
+# In[19]:
+
+
+a=x&y
+b=x|y
+print(a, b)
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
